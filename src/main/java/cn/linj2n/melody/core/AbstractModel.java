359a517f8eab83d3f7b4d158c4371db5c0e06147
linj2n <linj2n@163.com>
2018-10-14 21:54:55 +0800

    🎨 Update generation type
    NONE #31

--- src/main/java/cn/linj2n/melody/core/AbstractModel.java
+++ src/main/java/cn/linj2n/melody/core/AbstractModel.java
@@ -9,31 +9,12 @@ import java.util.Objects;
 public abstract class AbstractModel implements Comparable<AbstractModel>, Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Override
     public int compareTo(AbstractModel o) {
         return this.getId().compareTo(o.getId());
-    }
-
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        AbstractModel model = (AbstractModel) o;
-
-        if (!Objects.equals(model.getId(), this.id)) return false;
-
-        return true;
-    }
-
-    public int hashCode() {
-        return Objects.hashCode(id);
     }
 
     public Long getId() {
