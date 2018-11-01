4d5813873ff7d239394fc81d5f0b4cbffa24bf7b
linj2n <linj2n@163.com>
2018-11-01 20:36:16 +0800

    🚧 Update
    NONE #31

---
+++ src/main/java/cn/linj2n/melody/web/dto/PostDTO.java
@@ -0,0 +1,53 @@
+package cn.linj2n.melody.web.dto;
+
+import cn.linj2n.melody.domain.Category;
+import cn.linj2n.melody.domain.Tag;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Data
+public class PostDTO {
+
+    /**
+     * 文章 id
+     */
+    private Long id;
+    /**
+     * 文章标题
+     */
+    private String title;
+
+    /**
+     * 文章概要
+     */
+    private String summary;
+
+    /**
+     * 文章内容
+     */
+    private String content;
+
+    /**
+     * 文章状态
+     */
+    private String status;
+
+    /**
+     * 文章 url
+     */
+    private String url;
+
+    /**
+     * 文章所属的类别
+     */
+    private Set<Category> categories;
+
+    /**
+     * 文章所属的标签
+     */
+    private Set<Tag> tags;
+}
