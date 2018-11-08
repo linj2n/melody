3921360ce3a54aa107ae10a3e9969c840847351a
linj2n <linj2n@163.com>
2018-11-08 17:27:25 +0800

    🚧 Implement tag-related operation in back-end
    NONE #31

---
+++ src/main/java/cn/linj2n/melody/web/rest/TagResource.java
@@ -0,0 +1,54 @@
+package cn.linj2n.melody.web.rest;
+
+
+import cn.linj2n.melody.domain.Tag;
+import cn.linj2n.melody.repository.TagRepository;
+import cn.linj2n.melody.service.TagService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class TagResource {
+    private static final Logger logger = LoggerFactory.getLogger(TagResource.class);
+
+    private TagService tagService;
+
+    @Autowired
+    public TagResource(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+
+    @RequestMapping(value = "/v1/tags/",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Tag createTagByName(@RequestBody Tag tag) {
+        return tagService.createTag(tag.getName());
+    }
+    @RequestMapping(value = "/v1/tags/{tagId}",
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Tag updateTag(@RequestBody Tag tag) {
+        return tagService.updateTag(tag);
+    }
+
+    @RequestMapping(value = "/v1/tags/{tagId}",
+            method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> deleteTagById(@PathVariable(value = "tagId") Long tagId) {
+        logger.info("request to delete tag[id={}]",tagId);
+        if (!tagService.existsById(tagId)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        tagService.removeTagById(tagId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
