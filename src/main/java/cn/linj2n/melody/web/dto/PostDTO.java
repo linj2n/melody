d19fc2e16e3f8fe79774485c817ae73b710ba2f4
linj2n <linj2n@163.com>
2018-12-10 17:15:51 +0800

    🔥 Remove unnecessary codes
    NONE #31

--- src/main/java/cn/linj2n/melody/web/dto/PostDTO.java
+++ src/main/java/cn/linj2n/melody/web/dto/PostDTO.java
@@ -3,14 +3,8 @@ package cn.linj2n.melody.web.dto;
 import cn.linj2n.melody.domain.Category;
 import cn.linj2n.melody.domain.Tag;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.util.Date;
 import java.util.Set;
 
 
