7f5db0aaefa082346fccdde2df29f91fcabe59bf
linj2n <linj2n@163.com>
2019-05-12 21:45:26 +0800

    🚧 Update the tags component
    NONE #31

---
+++ app/src/views/category/components/ItemTabPane.vue
@@ -0,0 +1,197 @@
+<template>
+    <div class="ItemTabPane" >
+      <el-dropdown  
+        :trigger="trigger"
+        v-for="item in items"
+        :key="item.name"
+      >
+        <span class="el-tag el-tag--info">
+          {{item.name}}
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item @click.native="listPostByItemId(item.id)">
+            <span>
+              <i class="el-icon-view el-icon--left"></i>
+              查看文章
+            </span>
+          </el-dropdown-item>
+          <el-dropdown-item @click.native="handleEdit(item)">
+            <span>
+              <i class="el-icon-edit el-icon--left"></i>
+              编辑
+            </span>
+          </el-dropdown-item>
+          </el-dropdown-menu>
+      </el-dropdown>
+      <el-input
+        class="input-new-tag"
+        v-if="itemInputVisible"
+        v-model="itemInputValue"
+        ref="saveTagInput"
+        size="small"
+        @keyup.enter.native="handleItemInputConfirm"
+        @blur="handleItemInputConfirm"
+      >
+      </el-input>
+      <el-button v-else class="button-new-tag" size="small" @click="showTagInput">
+        + 新增标签
+      </el-button>
+      <el-dialog
+          :title="itemType"
+          :visible.sync="editDialogVisible"
+          >
+          <el-form label-position="left" label-width="50px" :model="currentEditingItem">
+            <el-form-item label="名称">
+              <el-input v-model="currentEditingItem.name"></el-input>
+            </el-form-item>
+          </el-form>
+          <span slot="footer" class="dialog-footer">
+            <el-popover
+              placement="top"
+              width="160"
+              v-model="deletePopoverVisible">
+              <p>确定删除吗？</p>
+              <div style="text-align: right; margin: 0">
+                <el-button size="mini" type="text" @click="deletePopoverVisible = false">取消</el-button>
+                <el-button type="primary" size="mini" @click="handleDeleteConfirm">确定</el-button>
+              </div>
+              <el-button slot="reference" type="danger" icon="el-icon-delete" >删 除</el-button>
+            </el-popover>
+            <el-button @click="editDialogVisible = false" style="margin-left: 10px;">
+              取 消
+            </el-button>
+            <el-button type="primary" @click="handleEditConfirm">
+              确 定
+            </el-button>
+          </span>
+        </el-dialog>
+      </div>
+</template>
+<script>
+export default {
+  name: 'ItemTabPane',
+  props: {
+    label: {
+      type: String,
+      default: 'Item'
+    },
+    name: {
+      type: String,
+      default: 'Pane'
+    },
+    itemType: {
+      type: String,
+      default: 'Edit item'
+    },
+    trigger: {
+      type: String,
+      default: 'click'
+    },
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      currentEditingItem: {
+        id: null,
+        name: ''
+      },
+      editDialogVisible: false,
+      deletePopoverVisible: false,
+      itemInputVisible: false,
+      itemInputValue: ''
+    }
+  },
+  methods: {
+    showTagInput() {
+      this.itemInputVisible = true;
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+    handleItemInputConfirm() {
+      const itemName = this.itemInputValue
+      if (itemName) {
+        let exited = this.items.find(item => item.name === itemName)
+        if (exited) {
+          this.$message({
+            message: '标签已存在，请勿重复添加',
+            type: 'warning'
+          })
+          return 
+        } else {
+          const newItem = {}
+          newItem['id'] = null
+          newItem['name'] = itemName
+          this.$emit('handleItemInputConfirm', newItem)
+        }
+      }
+      this.itemInputVisible = false
+      this.itemInputValue = ''
+    },
+    listPostByItemId(itemId) {
+      console.log('hit listPostByItemId in ItemTabPane')
+      this.$emit('listPostByItemId', itemId)
+    },
+    handleEdit(item) {
+      this.currentEditingItem = Object.assign({}, item)
+      this.editDialogVisible = true
+    },
+    handleDeleteConfirm() {
+      this.deletePopoverVisible = false
+      this.editDialogVisible = false
+      this.$emit('handleDeleteConfirm', Object.assign({}, this.currentEditingItem))
+    },
+    handleEditConfirm() {
+      this.deletePopoverVisible = false
+      this.editDialogVisible = false 
+      const newItem = Object.assign({}, this.currentEditingItem)
+      this.$emit('handleEditConfirm', newItem)
+    }
+  }
+}
+</script>
+<style>
+.el-tag {
+  display: inline-block;
+  margin-bottom: 1rem;
+  margin-right: 1rem;
+}
+.el-tag:hover {
+    background-color: rgba(64,158,255,.1);
+    display: inline-block;
+    padding: 0 10px;
+    height: 32px;
+    line-height: 30px;
+    font-size: 12px;
+    color: #409eff;
+    border-radius: 4px;
+    box-sizing: border-box;
+    border: 1px solid rgba(64,158,255,.2);
+    white-space: nowrap;
+    cursor: pointer;
+}
+.el-popper[x-placement^=bottom] {
+    margin-top: -10px;
+}
+.button-new-tag {
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.button-new-tag:hover{
+  background-color: rgba(103,194,58,.1);
+  border-color: rgba(103,194,58,.2);
+  color: #67c23a;
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  width: 90px;
+}
+</style>
--- app/src/views/category/index.vue
+++ app/src/views/category/index.vue
@@ -2,128 +2,50 @@
   <div class="card-container">
     <el-row>
         <el-card>
-          <el-tabs v-model="activePane" @tab-click="handleClick">
-          <el-tab-pane label="所有标签" name="allTagsPane">
-            <el-dropdown  
-              trigger="click"
-              v-for="tag in allTags"
-              :key="tag.name"
-            >
-              <span class="el-tag el-tag--info">
-                {{tag.name}}
-              </span>
-              <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item @click.native="listPostByTagId(tag.id)">
-                  <span>
-                    <i class="el-icon-view el-icon--left"></i>
-                    查看文章
-                  </span>
-                </el-dropdown-item>
-                <el-dropdown-item @click.native="handleEdit(tag)">
-                  <span>
-                    <i class="el-icon-edit el-icon--left"></i>
-                    编辑
-                  </span>
-                </el-dropdown-item>
-                </el-dropdown-menu>
-            </el-dropdown>
-            <el-input
-              class="input-new-tag"
-              v-if="tagInputVisible"
-              v-model="tagInputValue"
-              ref="saveTagInput"
-              size="small"
-              @keyup.enter.native="handleTagInputConfirm"
-              @blur="handleTagInputConfirm"
-            >
-            </el-input>
-            <el-button v-else class="button-new-tag" size="small" @click="showTagInput">
-              + 新增标签
-            </el-button>
+          <el-tabs v-model="activePane">
+          <el-tab-pane label="所有标签" name="tags">
+            <ItemTabPane
+              :items="allTags"
+              @handleItemInputConfirm="handleItemInputConfirm"
+              @listPostByItemId="listPostByItemId"
+              @handleDeleteConfirm="handleDeleteConfirm"
+              @handleEditConfirm="handleEditConfirm"
+            />
           </el-tab-pane>
-          <el-tab-pane label="所有分类" name="allCategoriesPane">
-            <el-dropdown  
-              trigger="click"
-              v-for="category in allCategories"
-              :key="category.name"
-            >
-              <span class="el-tag el-tag--info">
-                {{category.name}}
-              </span>
-              <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>
-                    <span @click="listPostByCategoryId(category.id)">
-                      <i class="el-icon-view el-icon--left"></i>
-                      查看文章
-                    </span>
-                  </el-dropdown-item>
-                  <el-dropdown-item>
-                    <span>
-                      <i class="el-icon-edit el-icon--left"></i>
-                      编辑
-                    </span>
-                  </el-dropdown-item>
-                </el-dropdown-menu>
-            </el-dropdown>
-            
+          <el-tab-pane label="所有分类" name="categories">
+            <ItemTabPane
+              :items="allCategories"
+              @handleItemInputConfirm="handleItemInputConfirm"
+              @listPostByItemId="listPostByItemId"
+              @handleDeleteConfirm="handleDeleteConfirm"
+              @handleEditConfirm="handleEditConfirm"
+            />
           </el-tab-pane>
             
           </el-tabs>
         </el-card>
-        <el-dialog
-          :title="typeTextMap[editDialogType]"
-          :visible.sync="editDialogVisible"
-          >
-          <el-form label-position="left" label-width="50px" :model="item">
-            <el-form-item label="名称">
-              <el-input v-model="item.name"></el-input>
-            </el-form-item>
-          </el-form>
-          <span slot="footer" class="dialog-footer">
-            <el-popover
-              placement="top"
-              width="160"
-              v-model="deletePopoverVisible">
-              <p>确定删除吗？</p>
-              <div style="text-align: right; margin: 0">
-                <el-button size="mini" type="text" @click="deletePopoverVisible = false">取消</el-button>
-                <el-button type="primary" size="mini" @click="handleDeleteConfirm">确定</el-button>
-              </div>
-              <el-button slot="reference" type="danger" icon="el-icon-delete" >删 除</el-button>
-            </el-popover>
-            <el-button @click="editDialogVisible = false">
-              取 消
-            </el-button>
-            <el-button type="primary" @click="handleEditConfirm">
-              确 定
-            </el-button>
-          </span>
-        </el-dialog>
     </el-row>
     
   </div>
 </template>
 
 <script>
-import { listAllCategories, listAllTags, createNewTag, updateTagOrCategory, removeTagOrCategory } from '@/api/post'
+import { listAllCategories, listAllTags, createNewTag, updateTagOrCategory, removeTagOrCategory, createNewTagOrCategory } from '@/api/post'
+import ItemTabPane from './components/ItemTabPane'
 export default {
+  components: { ItemTabPane },
   data() {
     return {
-      allTags: null,
-      allCategories: null,
-      activePane: 'allTagsPane',
-      tagInputVisible: false,
-      tagInputValue: '',
-      inputVisible: false,
-      inputValue: '',
-      deletePopoverVisible: false,
-      editDialogVisible: false,
-      editDialogType: '',
-      typeTextMap: {
-        tags: '标签',
-        categories: '分类',
+      allTags: [],
+      allCategories: [],
+      activePane: 'tags',
+      itemTypeMap: {
         allTagsPane: 'tags',
         allCategoriesPane: 'categories'
+      },
+      typeTextMap: {
+        tags: '标签',
+        categories: '分类'
       },
       item: {
         id: null,
@@ -135,53 +57,17 @@ export default {
     this.initTagsAndCategories()
   },
   methods: {
-    handleClick(tab, event) {
-      console.log(tab, event);
+    getCurrItemList() {
+      return this.activePane === 'tags' ? this.allTags : this.allCategories
     },
-    showInput() {
-      this.inputVisible = true;
-      this.$nextTick(_ => {
-        this.$refs.saveTagInput.$refs.input.focus()
+    handleItemInputConfirm(newItem) {
+      createNewTagOrCategory(this.activePane, newItem).then(response => {
+        this.getCurrItemList().push(response.data)
+        this.$message({
+          message: '添加成功',
+          type: 'success'
+        }) 
       })
-    },
-    handleInputConfirm() {
-      let inputValue = this.inputValue
-      if (inputValue) {
-        this.dynamicTags.push(inputValue)
-      }
-      this.inputVisible = false
-      this.inputValue = ''
-    },
-    showTagInput() {
-      this.tagInputVisible = true;
-      this.$nextTick(_ => {
-        this.$refs.saveTagInput.$refs.input.focus()
-      })
-    },
-    handleTagInputConfirm() {
-      let tagName = this.tagInputValue
-      if (tagName) {
-        let exited = this.allTags.find(tag => tag.name === tagName)
-        if (exited) {
-          this.$message({
-            message: '标签已存在',
-            type: 'danger'
-          })
-        } else {
-          var newTag = {}
-          newTag['id'] = null
-          newTag['name'] = tagName
-          createNewTag(newTag).then(response => {
-            this.allTags.push(response.data)
-            this.$message({
-              message: '添加成功',
-              type: 'success'
-            })
-          })
-        }
-      }
-      this.tagInputVisible = false
-      this.tagInputValue = ''
     },
     initTagsAndCategories() {
       listAllTags().then(response => {
@@ -191,41 +77,30 @@ export default {
         this.allCategories = response.data
       })
     },
-    listPostByTagId(id) {
-      this.$router.push({ path: '/posts', query: { tagId: id }})
+    listPostByItemId(id) {
+      console.log('hit listPostByItemId in index')
+      // TODO: 
+      const queryValue = {}
+      queryValue['tagId'] = id
+      this.$router.push({ path: '/posts', query: queryValue }) 
     },
-    listPostByCategoryId(id) {
-      this.$router.push({ path: '/posts', query: { categoryId: id }})
-    },
-    handleEdit(val) {
-      this.item = Object.assign({}, val)
-      this.editDialogType = this.typeTextMap[this.activePane]
-      this.editDialogVisible = true
-    },
-    handleEditConfirm() {
-      const type = this.editDialogType
-      const set = type === 'tags' ? this.allTags : this.allCategories
-      let item = Object.assign({}, this.item)
-      updateTagOrCategory(type, item).then(response => {
-        const newItem = response.data
-        const indexOfItem = set.findIndex(elem => elem.id === newItem.id)
-        set.splice(indexOfItem, 1, newItem)
-        this.editDialogVisible = false
+    handleEditConfirm(newItem) {
+      const itemList = this.getCurrItemList()
+      updateTagOrCategory(this.activePane, newItem).then(response => {
+        const item = response.data
+        const indexOfItem = itemList.findIndex(elem => elem.id === item.id)
+        itemList.splice(indexOfItem, 1, item)
         this.$message({
           message: response.message,
           type: 'success'
         })
       })
     },
-    handleDeleteConfirm() {
-      const type = this.editDialogType
-      const set = type === 'tags' ? this.allTags : this.allCategories
-      let item = Object.assign({}, this.item)
-      removeTagOrCategory(type, item).then(response => {
-        const indexOfItem = set.findIndex(elem => elem.id === item.id)
-        set.splice(indexOfItem, 1)
-        this.deletePopoverVisible = false
-        this.editDialogVisible = false
+    handleDeleteConfirm(item) {
+      const itemList = this.getCurrItemList()
+      removeTagOrCategory(this.activePane, Object.assign({}, item)).then(response => {
+        const indexOfItem = itemList.findIndex(e => e.id === item.id)
+        itemList.splice(indexOfItem, 1)
         this.$message({
           message: response.message,
           type: 'success'
@@ -240,49 +115,6 @@ export default {
   background-color: #f0f2f5;
   padding: 20px;
   min-height: calc(100vh - 84px);
-}
-.component-item{
-  min-height: 100px;
-}
-.el-tag {
-  display: inline-block;
-  margin-bottom: 1rem;
-  margin-right: 1rem;
-}
-.el-tag:hover {
-    background-color: rgba(64,158,255,.1);
-    display: inline-block;
-    padding: 0 10px;
-    height: 32px;
-    line-height: 30px;
-    font-size: 12px;
-    color: #409eff;
-    border-radius: 4px;
-    box-sizing: border-box;
-    border: 1px solid rgba(64,158,255,.2);
-    white-space: nowrap;
-    cursor: pointer;
-}
-.el-popper[x-placement^=bottom] {
-    margin-top: -10px;
-}
-.button-new-tag {
-  height: 32px;
-  line-height: 30px;
-  padding-top: 0;
-  padding-bottom: 0;
-}
-.button-new-tag:hover{
-  background-color: rgba(103,194,58,.1);
-  border-color: rgba(103,194,58,.2);
-  color: #67c23a;
-  height: 32px;
-  line-height: 30px;
-  padding-top: 0;
-  padding-bottom: 0;
-}
-.input-new-tag {
-  width: 90px;
 }
 </style>
 
