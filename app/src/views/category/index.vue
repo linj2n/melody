14e9b521039cf7bbdcfc35a056be42789d090be9
linj2n <linj2n@163.com>
2019-05-16 19:55:53 +0800

    🎨 Update
    NONE #31

--- app/src/views/category/index.vue
+++ app/src/views/category/index.vue
@@ -80,7 +80,9 @@
           </div>
           <el-button slot="reference" type="danger">删 除</el-button>
         </el-popover>
-        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button
+style="margin-left: 10px"
+@click="editDialogVisible = false">取 消</el-button>
         <el-button
 type="primary"
 @click="handleEditConfirm(currentItem)">确 定</el-button>
