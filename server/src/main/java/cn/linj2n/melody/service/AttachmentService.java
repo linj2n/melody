3e7c1718bd0a720eb0b9274775973262ffba14fb
linj2n <linj2n@163.com>
2019-05-29 22:53:50 +0800

    🎨 Reformat code
    NONE #31

--- server/src/main/java/cn/linj2n/melody/service/AttachmentService.java
+++ server/src/main/java/cn/linj2n/melody/service/AttachmentService.java
@@ -1,14 +1,13 @@
 package cn.linj2n.melody.service;
 
 import cn.linj2n.melody.domain.Attachment;
+import cn.linj2n.melody.web.dto.AttachmentDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface AttachmentService {
-
-    Page<Attachment> listAllAttachmentByPage(Pageable pageable);
 
     boolean checkIfExistsByName(String name);
 
@@ -29,4 +28,9 @@ public interface AttachmentService {
     Attachment changeMimeType(Attachment newAttachment);
 
     Page<Attachment> queryAttachmentsByNameContaining(String name, Pageable pageable);
+
+    void updateAttachment(AttachmentDTO attachmentDTO);
+
+    Page<AttachmentDTO> listAllAttachmentByPage(Pageable pageable);
+
 }
