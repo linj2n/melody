248d0018f735da3b23f62e4a2148bc3a7c0f5f66
linj2n <linj2n@163.com>
2019-05-29 22:54:26 +0800

    🚧 Update attachments resource
    NONE #31

--- server/src/main/java/cn/linj2n/melody/web/dto/AttachmentDTO.java
+++ server/src/main/java/cn/linj2n/melody/web/dto/AttachmentDTO.java
@@ -1,4 +1,10 @@
 package cn.linj2n.melody.web.dto;
 
+import cn.linj2n.melody.domain.QiniuFile;
+
 public class AttachmentDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private QiniuFileDTO qiniuFileDTO;
 }
