1a817dc4e2df3427c61b4601d19e0dd0400705c8
linj2n <linj2n@163.com>
2019-06-16 23:15:10 +0800

    🚧 Enable Scheduling
    NONE #31

--- server/src/main/java/cn/linj2n/melody/config/AsyncConfig.java
+++ server/src/main/java/cn/linj2n/melody/config/AsyncConfig.java
@@ -7,12 +7,14 @@ import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
+@EnableScheduling
 public class AsyncConfig implements AsyncConfigurer {
     private final Logger logger = LoggerFactory.getLogger(AsyncConfig.class);
 
