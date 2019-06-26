e885dedeee347f731003b1e07c5ab721c0171c3e
linj2n <linj2n@163.com>
2019-06-26 20:22:30 +0800

    🚧 Update setting api
    NONE #31

--- app/src/api/setting.js
+++ app/src/api/setting.js
@@ -1,9 +1,23 @@
 import request from '@/utils/request'
+import qs from 'qs'
 
 export function fetchAllOptions() {
   return request({
     url: '/api/v1/config',
     method: 'get'
+  })
+}
+
+export function fetchOptions(names) {
+  return request({
+    url: '/api/v1/config',
+    method: 'get',
+    params: {
+      optionName: names
+    },
+    paramsSerializer: params => {
+      return qs.stringify(params, { arrayFormat: 'repeat' })
+    }
   })
 }
 
