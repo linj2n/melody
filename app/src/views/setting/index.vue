f5097bb94c343d6e0b3a112578bcbcbbd00665d0
linj2n <linj2n@163.com>
2019-06-26 20:21:49 +0800

    🚧 Update setting-view
    NONE #31

--- app/src/views/setting/index.vue
+++ app/src/views/setting/index.vue
@@ -5,55 +5,59 @@
         <el-tabs v-model="activeName">
           <el-tab-pane label="站点配置" name="SiteSetting">
             <el-form
+              v-model="siteSetting"
               label-position="left"
               label-width="80px"
               class="option-form"
             >
               <el-form-item label="标 题">
-                <el-input v-model="options.siteTitle" />
+                <el-input v-model="siteSetting.siteTitle" />
               </el-form-item>
               <el-form-item label="副标题">
-                <el-input v-model="options.siteSubtitle" />
+                <el-input v-model="siteSetting.siteSubtitle" />
               </el-form-item>
               <el-form-item label="描述">
-                <el-input v-model="options.siteDescription" type="textarea" />
+                <el-input
+                  v-model="siteSetting.siteDescription"
+                  type="textarea"
+                />
               </el-form-item>
               <el-form-item size="large">
-                <el-button 
-type="primary" 
-@click="updateOptions"
-                >保存</el-button
-                >
+                <el-button type="primary" @click="updateOptions(siteSetting)">
+                  保存
+                </el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
           <el-tab-pane label="社交资料" name="SocialSetting">
             <el-form
+              v-model="socialSetting"
               label-position="left"
-              label-width="80px"
+              label-width="100px"
               class="option-form"
             >
               <el-form-item label="作者名称">
-                <el-input v-model="options.siteAuthorName" />
+                <el-input v-model="socialSetting.siteAuthorName" />
               </el-form-item>
-              <el-form-item label="头像">
-                <el-input v-model="options.siteAuthorAvatar" />
+              <el-form-item label="头 像">
+                <el-input v-model="socialSetting.siteAuthorAvatar" />
+              </el-form-item>
+              <el-form-item label="Email">
+                <el-input v-model="socialSetting.social_email" />
               </el-form-item>
               <el-form-item label="Github">
-                <el-input v-model="options.social_github" />
+                <el-input v-model="socialSetting.social_github" />
               </el-form-item>
-              <el-form-item label="Email地址">
-                <el-input v-model="options.social_email" />
+              <el-form-item label="StackOverFlow">
+                <el-input v-model="socialSetting.social_stackOverflow" />
               </el-form-item>
-              <el-form-item label="微博地址">
-                <el-input v-model="options.social_weibo" />
+              <el-form-item label="微 博">
+                <el-input v-model="socialSetting.social_weibo" />
               </el-form-item>
               <el-form-item size="large">
-                <el-button 
-type="primary" 
-@click="updateOptions"
-                >保存</el-button
-                >
+                <el-button type="primary" @click="updateOptions(socialSetting)">
+                  保存
+                </el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -64,28 +68,46 @@ type="primary"
 </template>
 
 <script>
-import { fetchAllOptions, updateOptions } from '@/api/setting'
+import { updateOptions, fetchOptions } from '@/api/setting'
 
 export default {
   name: 'Setting',
   data() {
     return {
       activeName: 'SiteSetting',
-      options: null
+      siteSetting: {
+        siteTitle: null,
+        siteSubtitle: null,
+        siteDescription: null
+      },
+      socialSetting: {
+        siteAuthorName: null,
+        siteAuthorAvatar: null,
+        social_github: null,
+        social_email: null,
+        social_weibo: null,
+        social_stackOverflow: null
+      }
     }
   },
   created() {
-    this.fetchAllOptions()
+    this.fetchAllSettingOptions()
   },
   methods: {
-    fetchAllOptions() {
-      fetchAllOptions().then(response => {
-        this.options = response.data
+    fetchAllSettingOptions() {
+      fetchOptions(Object.keys(this.siteSetting)).then(response => {
+        this.siteSetting = response.data
+      })
+      fetchOptions(Object.keys(this.socialSetting)).then(response => {
+        this.socialSetting = response.data
       })
     },
-    updateOptions() {
-      updateOptions(Object.assign({}, this.options)).then(response => {
-        console.log('success')
+    updateOptions(options) {
+      updateOptions(Object.assign({}, options)).then(response => {
+        this.$message({
+          message: response.message,
+          type: 'success'
+        })
       })
     }
   }
