8fa5c5faa8f37f466fa7c8acd54817723ad7e586
linj2n <linj2n@163.com>
2019-07-14 22:35:33 +0800

    🚧 Update exception handler
    NONE #31

--- server/src/main/java/cn/linj2n/melody/web/errors/RestResponseEntityExceptionHandler.java
+++ server/src/main/java/cn/linj2n/melody/web/errors/RestResponseEntityExceptionHandler.java
@@ -1,31 +1,34 @@
 package cn.linj2n.melody.web.errors;
 
+import cn.linj2n.melody.web.dto.ResponseDTO;
+import cn.linj2n.melody.web.utils.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler  {
 
     @Autowired
     private MessageSource message;
 
-    public RestResponseEntityExceptionHandler() {
-        super();
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public ResponseDTO<?> processValidationError(MethodArgumentNotValidException ex) {
+        List<FieldErrorDTO> errorDTOS =
+                ex.getBindingResult().getFieldErrors()
+                        .stream()
+                        .map(error -> new FieldErrorDTO(error.getField(), error.getDefaultMessage()))
+                        .collect(Collectors.toList());
+        return ResponseBuilder.buildFailedResponse("字段错误", errorDTOS);
     }
-
-    // 409
-//    @ExceptionHandler({ UserAlreadyExistException.class})
-//    public ResponseEntity<Object> handleUserAlreadyExist(final RuntimeException ex, final WebRequest request) {
-//        logger.error("409 Status Code", ex);
-//        final ErrorResponse bodyOfResponse =
-//                new ErrorResponse(HttpStatus.CONFLICT.value(), "UserAlreadyExist", message.getMessage("message.emailOrLogin.alreadyExist",null,request.getLocale()));
-//        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
-//    }
 }
