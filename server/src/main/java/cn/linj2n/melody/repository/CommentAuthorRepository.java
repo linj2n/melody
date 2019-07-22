b9241f02ed00c0b861dcd5343840160603a1ff8a
linj2n <linj2n@163.com>
2019-07-22 21:09:57 +0800

    🚧 Update comment related repository classes
    NONE #31

---
+++ server/src/main/java/cn/linj2n/melody/repository/CommentAuthorRepository.java
@@ -0,0 +1,18 @@
+package cn.linj2n.melody.repository;
+
+import cn.linj2n.melody.domain.CommentAuthor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CommentAuthorRepository extends JpaRepository<CommentAuthor, Long> {
+
+    Optional<CommentAuthor> findOptionalById(Long id);
+
+    Optional<CommentAuthor> findOptionalByEmail(String email);
+
+//    Long getAuthorIdByEmail(String email);
+}
+
--- server/src/main/java/cn/linj2n/melody/repository/CommentRepository.java
+++ server/src/main/java/cn/linj2n/melody/repository/CommentRepository.java
@@ -1,7 +1,36 @@
 package cn.linj2n.melody.repository;
 
+import cn.linj2n.melody.domain.Comment;
+import cn.linj2n.melody.domain.enumeration.CommentStatus;
+import cn.linj2n.melody.domain.enumeration.CommentType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CommentRepository {
+public interface CommentRepository extends JpaRepository<Comment, Long>{
+
+    Optional<Comment> findOptionalById(Long id);
+
+    Page<Comment> findAllByPostIdAndCommentTypeAndCommentStatus(Long id, CommentType commentType, CommentStatus commentStatus, Pageable pageable);
+
+    Page<Comment> findAllByPostIdAndParentCommentIdAndCommentStatus(Long postId, Long parentCommentId, CommentStatus commentStatus, Pageable pageable);
+
+    @Modifying
+    @Query(
+            value = " UPDATE comment SET parent_post_id = :postId, parent_comment_id = :parentCommentId, author_id = :authorId, reply_to_author_id = :replyToAuthorId" +
+                    " WHERE id = :commentId "
+            ,nativeQuery = true
+    )
+    void updateCommentDetail(@Param("commentId") Long commentId,
+                                    @Param("postId") Long postId,
+                                    @Param("authorId") Long authorId,
+                                    @Param("replyToAuthorId") Long replyToAuthorId,
+                                    @Param("parentCommentId") Long parentCommentId);
 }
