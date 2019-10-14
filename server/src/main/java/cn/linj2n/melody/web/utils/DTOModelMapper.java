9ba0e677e3ea7f3def6fed35482cf2645380513d
linj2n <linj2n@163.com>
2019-10-14 23:26:29 +0800

    🚧 Update DTOModelMapper
    NONE #31

--- server/src/main/java/cn/linj2n/melody/web/utils/DTOModelMapper.java
+++ server/src/main/java/cn/linj2n/melody/web/utils/DTOModelMapper.java
@@ -2,7 +2,8 @@ package cn.linj2n.melody.web.utils;
 
 import cn.linj2n.melody.config.MelodyProperties;
 import cn.linj2n.melody.domain.*;
-import cn.linj2n.melody.domain.enumeration.CommentAuthorRole;
+import cn.linj2n.melody.security.AuthoritiesConstants;
+import cn.linj2n.melody.security.SecurityUtil;
 import cn.linj2n.melody.utils.QiniuUtil;
 import cn.linj2n.melody.web.dto.AttachmentDTO;
 import cn.linj2n.melody.web.dto.PostDTO;
@@ -11,11 +12,13 @@ import cn.linj2n.melody.web.dto.UserDTO;
 import cn.linj2n.melody.web.dto.comment.CommentAuthorDTO;
 import cn.linj2n.melody.web.dto.comment.CommentDTO;
 import cn.linj2n.melody.web.dto.comment.ReplyToCommentDTO;
-import org.jsoup.Jsoup;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.ZonedDateTime;
 import java.util.stream.Collectors;
 
 @Component
@@ -23,17 +26,19 @@ public class DTOModelMapper {
 
     private static final int MAX_LENGTH_OF_CONTENT_PREVIEW = 300;
 
-    private ModelMapper modelMapper;
-
-    private ViewUtils viewUtils;
-
-    private MelodyProperties melodyProperties;
+    private ModelMapper modelMapper = new ModelMapper();
 
     @Autowired
-    public DTOModelMapper(ModelMapper modelMapper, ViewUtils viewUtils, MelodyProperties melodyProperties) {
-        this.modelMapper = modelMapper;
-        this.viewUtils = viewUtils;
-        this.melodyProperties = melodyProperties;
+    private ViewUtils viewUtils;
+
+    @Autowired
+    private MelodyProperties melodyProperties;
+
+    public DTOModelMapper() {
+        this.modelMapper.addMappings(getCommentMap());
+        this.modelMapper.addMappings(getCommentAuthorMap());
+        this.modelMapper.addMappings(getReplyToCommentMap());
+
     }
 
     public PostDTO convertToDTO(final Post post) {
@@ -51,16 +56,6 @@ public class DTOModelMapper {
         postDTO.setContentPreview(postDTO.getSummary());
         return postDTO;
     }
-
-//    private String getContentPreview(PostDTO postDTO) {
-//        if (!postDTO.getSummary().isEmpty()) {
-//            return postDTO.getSummary();
-//        }
-//        String contentText = Jsoup.parse(postDTO.getContent()).text();
-//        return contentText.length() > MAX_LENGTH_OF_CONTENT_PREVIEW
-//                ? contentText.substring(0, MAX_LENGTH_OF_CONTENT_PREVIEW)
-//                : contentText;
-//    }
 
     public Post convertToEntity(PostDTO postDTO) {
         return modelMapper.map(postDTO, Post.class);
@@ -125,37 +120,67 @@ public class DTOModelMapper {
 
 
     public CommentDTO convertToDTO(Comment comment) {
-        CommentDTO commentDTO = new CommentDTO();
-        commentDTO.setId(comment.getId());
-        commentDTO.setCreatedTime(comment.getCreatedAt().toEpochSecond());
-        commentDTO.setAuthor(convertToDTO(comment.getAuthor()));
-        commentDTO.setContent(comment.getContent());
-        commentDTO.setReplyCount(comment.getReplyCount());
-        return commentDTO;
+        return modelMapper.map(comment, CommentDTO.class);
     }
 
-    public CommentAuthorDTO convertToDTO(CommentAuthor commentAuthor) {
-       CommentAuthorDTO commentAuthorDTO = new CommentAuthorDTO();
-       commentAuthorDTO.setId(commentAuthor.getId());
-       commentAuthorDTO.setAvatar(commentAuthor.getAvatar());
-       commentAuthorDTO.setName(commentAuthor.getName());
-       commentAuthorDTO.setPostAuthor(checkIfAdminAuthor(commentAuthor));
-       return commentAuthorDTO;
+    public CommentAuthorDTO convertToCommentAuthorDTO(User user) {
+        return modelMapper.map(user, CommentAuthorDTO.class);
     }
 
     public ReplyToCommentDTO convertToReplyDTO(Comment comment) {
-        ReplyToCommentDTO reply = new ReplyToCommentDTO();
-        reply.setId(comment.getId());
-        reply.setContent(comment.getContent());
-        reply.setAuthor(convertToDTO(comment.getAuthor()));
-        reply.setReplyToAuthor(convertToDTO(comment.getReplyToAuthor()));
-        reply.setCreatedTime(comment.getCreatedAt().toEpochSecond());
-        return reply;
+        return modelMapper.map(comment, ReplyToCommentDTO.class);
     }
 
-
-    private boolean checkIfAdminAuthor(CommentAuthor commentAuthor) {
-        return commentAuthor.getRole().equals(CommentAuthorRole.ADMIN);
+    private PropertyMap<Comment, ReplyToCommentDTO> getReplyToCommentMap() {
+        return new PropertyMap<Comment, ReplyToCommentDTO>() {
+            @Override
+            protected void configure() {
+                map().setId(source.getId());
+                map().setContent(source.getContent());
+                using(zonedDateTimeToLongConverter()).map(source.getCreatedAt()).setCreatedTime(null);
+            }
+        };
     }
+
+    private PropertyMap<Comment, CommentDTO> getCommentMap() {
+        return new PropertyMap<Comment, CommentDTO>() {
+            @Override
+            protected void configure() {
+                map().setId(source.getId());
+                map().setContent(source.getContent());
+                map().setReplyCount(source.getReplyCount());
+                using(zonedDateTimeToLongConverter())
+                        .map(source.getCreatedAt()).setCreatedTime(null);
+
+
+            }
+        };
+    }
+
+    private PropertyMap<User, CommentAuthorDTO> getCommentAuthorMap() {
+        return new PropertyMap<User, CommentAuthorDTO>() {
+            @Override
+            protected void configure() {
+                map().setId(source.getId());
+                map().setAvatar(source.getAvatarUrl());
+                using(usernameConverter())
+                        .map(source).setName(null);
+                using((Converter<Object, Boolean>) context -> SecurityUtil.isCurrentUserInRole(AuthoritiesConstants.ADMIN))
+                        .map().setPostAuthor(false);
+            }
+        };
+    }
+
+    private Converter<ZonedDateTime, Long> zonedDateTimeToLongConverter() {
+        return context -> context.getSource().toEpochSecond();
+    }
+
+    private Converter<User, String> usernameConverter() {
+        return context -> {
+            User user = context.getSource();
+            return user.getUsername() != null ? user.getUsername() : user.getLogin();
+        };
+    }
+
 
 }
