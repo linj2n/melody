22e8e438aa658098d95383683a7ab5a19b28b5fa
linj2n <linj2n@163.com>
2019-11-01 21:24:40 +0800

    🚧 Add comment.js
    NONE #31

--- comments-app/src/api/comment.js
+++ comments-app/src/api/comment.js
@@ -1,13 +1,38 @@
 import request from '@/utils/request'
 
 export function listAllPostComments (postId, pager, sort) {
-  console.log(pager)
   return request({
     url: `/api/v1/posts/${postId}/comments`,
     method: 'get',
     params: {
       page: pager.current > 0 ? pager.current - 1 : pager.current,
       size: pager.pageSize,
+      sort: sort
+    }
+  })
+}
+
+export function replyToPost (postId, comment) {
+  return request({
+    url: `/api/v1/posts/${postId}/comments`,
+    method: 'post',
+    data: comment
+  })
+}
+
+export function replyToComment (postId, parentCommentId, reply) {
+  return request({
+    url: `/api/v1/posts/${postId}/comments/${parentCommentId}/replies`,
+    method: 'post',
+    data: reply
+  })
+}
+
+export function listChildComments (postId, commentId, sort) {
+  return request({
+    url: `/api/v1/posts/${postId}/comments/${commentId}/replies`,
+    method: 'get',
+    params: {
       sort: sort
     }
   })
