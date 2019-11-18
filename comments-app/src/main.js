27ddd05bc2f2126181642a5a294f04d446924a5a
linj2n <linj2n@163.com>
2019-11-18 23:39:58 +0800

    ➕ Add vuex dependency
    NONE #31

--- comments-app/src/main.js
+++ comments-app/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 // import Button from 'ant-design-vue/lib/button'
 // import 'ant-design-vue/dist/antd.css'
-import { Button, Tooltip, Avatar, Icon, Comment, List, Input, Form, Row, Col, Divider, Spin } from 'ant-design-vue'
+import { Button, Tooltip, Avatar, Icon, Comment, List, Input, Form, Row, Col, Divider, Spin, Modal, message } from 'ant-design-vue'
 import App from './App'
+import store from './store'
 // import Antd from 'ant-design-vue'
 
 // Vue.component(Button.name, Button, Avatar, Icon, Tooltip, Tooltip.name, Comment, Comment.name)
@@ -18,9 +19,12 @@ Vue.use(Row)
 Vue.use(Col)
 Vue.use(Divider)
 Vue.use(Spin)
+Vue.use(Modal)
+
+Vue.prototype.$message = message
 
 Vue.config.productionTip = false
-
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
